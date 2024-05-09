a45d60db960c6ce8667fd16f9aeefbc74e21080e
Nkemjika1divine <cndivine@gmail.com>
2024-05-09 01:12:19 +0000

    Caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -13,7 +13,7 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """Adds a key value pair to self.cache_data"""
-        if key is None of item is None:
+        if key is None or item is None:
             return
         data = {key: item}
         self.cache_data.update(data)
